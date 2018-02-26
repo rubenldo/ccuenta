0e13c7b8f4537aa757c1f9b36e305165b3ffa154
Ruben <Ruben@192.168.1.64>
2018-02-26 01:59:27 +0100

    nueva

---
+++ CCuenta/src/TestCcuentaRLD.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCcuentaRLD {
+
+	@Test
+	void test() {
+		fail("No implementado aun");
+	}
+
+}
+iuypkpip
